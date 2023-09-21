9f548a5aa2fbc60bacc8bd711e4200b86c336964
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-09-21 10:28:26 -0500

    register API and register component start

--- src/client/API/index.js
+++ src/client/API/index.js
@@ -13,3 +13,25 @@ export async function fetchAllReviews() {
     }
 }
 
+// Registering a new user
+export async function fetchNewUser(username, password) {
+  try {
+    const response = await fetch('http://localhost:3000/api/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: `${username}`,
+          password: `${password}`,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
