b18a931da1dc53dc13719e4128d35d90dee8481f
kmorr102 <kendranmorris@gmail.com>
2023-09-21 20:11:26 -0500

    updates from class

---
+++ src/server/api/utils.js
@@ -0,0 +1,13 @@
+//require user function
+function requireUser(req,res,next) {
+    if(!req.user) {
+      res.status(401);
+      next({
+        name: "MissingUserError",
+        message:"You must be logged in to perform this action"
+      });
+    }
+    next()
+  };
+
+  module.export= requireUser
