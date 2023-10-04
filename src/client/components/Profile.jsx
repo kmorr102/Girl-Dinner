9364b24f195b88562ae85589b89cacefae539e26
kmorr102 <kendranmorris@gmail.com>
2023-10-03 20:16:30 -0500

    fixed small error

--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getAllReviews } from '../API';
+//import { getAllReviews } from '../API';
 import { IoApertureSharp } from 'react-icons/io5';
 
 export default function Profile() {
