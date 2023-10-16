c94ac7d3d890e7e9edaeed797f5965e4df61ed94
kmorr102 <kendranmorris@gmail.com>
2023-10-16 14:35:00 -0500

    review drop down

--- src/client/components/CreateReviewForm.jsx
+++ src/client/components/CreateReviewForm.jsx
@@ -16,9 +16,60 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CreateIcon from '@mui/icons-material/Create';
 import { NineKPlusOutlined } from "@mui/icons-material";
 
+import { styled, alpha } from '@mui/material/styles';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Divider from '@mui/material/Divider';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import * as React from 'react';
+
+
 import FormControl from 'react-bootstrap/FormControl'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
 
 function Copyright(props) {
   return (
@@ -54,6 +105,14 @@ export default function CreateReview({currentUser}){
     const [rating, setRating] = useState(null);
     const [hover, setHover] = useState(null);
     const [restaurantId, setRestaurantId] = useState(null);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+    const handleClose = () => {
+    setAnchorEl(null);
+  };
 
 
     const Navigate= useNavigate();
@@ -130,7 +189,42 @@ export default function CreateReview({currentUser}){
             Write Review
           </Typography>
           
-          <FormControl fullWidth>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <Button
+                id="demo-customized-button"
+                aria-controls={open ? 'demo-customized-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                variant="contained"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+              >
+                Select a restaurant
+              </Button>
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+              >
+                {searchParams &&
+                  searchParams.map((restaurant) => (
+                    <MenuItem
+                      key={restaurant.id}
+                      value={restaurant.id}
+                      onClick={() => handleSelectRestaurant(restaurant.id)}
+                    >
+                      {restaurant.name}
+                    </MenuItem>
+                  ))}
+              </StyledMenu>
+
+
+          {/* <FormControl fullWidth>
           <FloatingLabel htmlFor="restaurant-select">Select a restaurant</FloatingLabel>
           <Form.Select
             value={restaurantId}
@@ -140,18 +234,21 @@ export default function CreateReview({currentUser}){
               id: 'restaurant-select',
             }}
           >
-            <MenuItem value="" disabled>
+            <Dropdown>
+            <Dropdown.Menu value="" disabled>
               Select a restaurant
-            </MenuItem>
-            {searchParams.map((restaurant) => (
-              <MenuItem key={restaurant.id} value={restaurant.id}>
+            </Dropdown.Menu>
+            {searchParams && searchParams.map((restaurant) => (
+              <Dropdown.Item key={restaurant.id} value={restaurant.id}>
                 {restaurant.name}
-              </MenuItem>
+              </Dropdown.Item>
+             
             ))}
+            </Dropdown>
           </Form.Select>
-        </FormControl>
+        </FormControl> */}
 
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          
             <TextField
               margin="normal"
               required
